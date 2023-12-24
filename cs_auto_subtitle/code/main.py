140bcde493aa95e0d61ce858d1931e0345a28313
TRG <roupertrg@gmail.com>
2023-12-24 15:48:56 +0800

    remove

--- cs_auto_subtitle/code/main.py
+++
@@ -1,8 +0,0 @@
-import srt_extraction
-import video_subtitle_merging
-
-if __name__ == "__main__":
-    video_file_path = r"..\video_inital"
-    srt_file_path = r"..\srt_files"
-    output_file_path = r"..\videos_subtitled"
-    srt_extraction.main(video_file_path,srt_file_path,output_file_path)
